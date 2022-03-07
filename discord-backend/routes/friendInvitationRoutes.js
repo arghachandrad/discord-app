5ec9695f1476e95838745759f61ea0a3e748924c
arghachdas <arghachandra@technoexponent.com>
2022-03-07 21:11:23 +0530

    reject invitation api done, using real time changes

--- discord-backend/routes/friendInvitationRoutes.js
+++ discord-backend/routes/friendInvitationRoutes.js
@@ -9,11 +9,28 @@ const postFriendInvitationSchema = Joi.object({
   targetMailAddress: Joi.string().email(),
 })
 
+const inviteDecisionSchema = Joi.object({
+  id: Joi.string().required(),
+})
+
 router.post(
   "/invite",
   verifyToken,
   validator.body(postFriendInvitationSchema),
   friendInvitationControllers.controllers.postInvite
+)
+
+router.post(
+  "/accept",
+  verifyToken,
+  validator.body(inviteDecisionSchema),
+  friendInvitationControllers.controllers.postAccept
+)
+router.post(
+  "/reject",
+  verifyToken,
+  validator.body(inviteDecisionSchema),
+  friendInvitationControllers.controllers.postReject
 )
 
 module.exports = router
