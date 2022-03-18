4e5a574c4208e396aa0e304753d4a50a4d2264ce
arghachdas <arghachandra@technoexponent.com>
2022-03-18 23:44:23 +0530

    Once one user try to join, he is asking other users to prepare for connection

--- discord-backend/socketHandlers/roomJoinHandler.js
+++ discord-backend/socketHandlers/roomJoinHandler.js
@@ -14,6 +14,17 @@ const roomJoinHandler = (socket, data) => {
 
   serverStore.joinActiveRoom(roomId, participantDetails)
 
+  // send information to users in room, that they should prepare for incoming connection
+  roomDetails.participants.forEach((participant) => {
+    if (participant.socketId !== participantDetails.socketId) {
+      // send the event ONLY to other users
+      // to every participants in this room, tell to prepare
+      socket.to(participant.socketId).emit("conn-prepare", {
+        connUserSocketId: participantDetails.socketId, // new joinee socketId, sending to other users already in room
+      })
+    }
+  })
+
   // reusing func, used for updating to other users in room that new participants join
   roomsUpdates.updateRooms()
 }
