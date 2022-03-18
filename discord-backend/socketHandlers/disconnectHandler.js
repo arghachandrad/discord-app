af00ae422d15c362a2b26b671d8a1e720f8db95a
arghachdas <arghachandra@technoexponent.com>
2022-03-18 16:39:11 +0530

    leave room while browser disconnect

--- discord-backend/socketHandlers/disconnectHandler.js
+++ discord-backend/socketHandlers/disconnectHandler.js
@@ -1,6 +1,19 @@
 const serverStore = require("../serverStore")
+const roomLeaveHandler = require("./roomLeaveHandler")
 
 const disconnectHandler = (socket) => {
+  // before disconnect check if this user in Room or not
+  const activeRooms = serverStore.getActiveRooms()
+
+  activeRooms.forEach((activeRoom) => {
+    const userInRoom = activeRoom.participants.some(
+      (participant) => participant.socketId === socket.id
+    )
+
+    if (userInRoom) {
+      roomLeaveHandler(socket, { roomId: activeRoom.roomId })
+    }
+  })
   serverStore.removeConnectedUser(socket.id)
 }
 
