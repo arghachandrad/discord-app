9bef869e113379a2ef5c5f931a4544f214a67633
arghachdas <arghachandra@technoexponent.com>
2022-03-18 16:46:36 +0530

    Initial update of the active rooms

--- discord-backend/socketHandlers/newConnectionHandler.js
+++ discord-backend/socketHandlers/newConnectionHandler.js
@@ -1,5 +1,6 @@
 const serverStore = require("../serverStore")
 const friendsUpdate = require("./updates/friends")
+const roomsUpdate = require("./updates/rooms")
 
 // executes when you landed on the app and new socket connections estalished
 const newConnectionHandler = async (socket, io) => {
@@ -15,6 +16,11 @@ const newConnectionHandler = async (socket, io) => {
 
   // update friends list
   friendsUpdate.updateFriends(userDetails.userId)
+
+  // on refresh or initially, room data emiting related to particular user
+  setTimeout(() => {
+    roomsUpdate.updateRooms(socket.id)
+  }, [500])
 }
 
 module.exports = newConnectionHandler
